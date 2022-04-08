b1ae9844d2eb48b68867339aa48754d319cafbbb
AlexanderCherniy <scernysev389@gmail.com>
2022-04-08 15:32:34 +0300

    new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Profile from './components/Profile';
 import SideBar from './components/SideBar';
 import './Nullstyle.css';
-
 const App = ()=> {
     return (
       <div className="wrapper">
